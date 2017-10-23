802db22bc2f0d6273372031b674829ceee098257
GeorgeKihara <gegohcomp@gmail.com>
2017-10-23 23:00:03 +0300

    models for the database

---
+++ models/country.js
@@ -0,0 +1,13 @@
+const database = require ('../configs/database');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+mongoose.connect(database);
+
+var CountrySchema = new Schema({
+    country_id  : {type:Number, unique:true, required:true},
+    country_name : {type:String, required:true},
+
+});
+
+var Country = mongoose.exports = mongoose.model("Bot", CountrySchema);
---
+++ models/developer.js
@@ -0,0 +1,14 @@
+const database = require ('../configs/database');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+mongoose.connect(database);
+
+var DeveloperSchema = new Schema({
+    developer_id  : {type:Number, unique:true, required:true},
+    developer_name : {type:String, required:true},
+    country : {type:String, required:true},
+
+});
+
+var Bot = mongoose.exports = mongoose.model("Bot", BotsSchema);
